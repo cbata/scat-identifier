67821a1da7600c676b917a908b7e34a0c837f4e7
Chris Bata <cjbata@gmail.com>
2020-08-24 12:56:13 -0600

    initial commit

---
+++ Scat Detective/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scat Detective
+//
+//  Created by Chris Bata on 8/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
